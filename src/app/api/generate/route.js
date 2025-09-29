caa639b237bc630315aa4af7ddf54bb5f4bd45f3
Joanne Affolter <joanne.affolter@epfl.ch>
2025-09-29 20:59:15 +0200

    test

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+{/**import { NextResponse } from "next/server";
 
 
 function rewriteImages(obj, baseUrl) {
@@ -78,4 +78,39 @@ export async function POST(request) {
       { status: 500 }
     );
   }
+}
+
+*/}
+
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req) {
+  const data = await req.json();
+
+  const res = await fetch(`https://render.carbone.io/render/5ecb030519ec4baa303918414936dec8cc4ef8b902e26c4d859422c8aa9975d2`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer test_eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxMjU1ODI5NjMwNDA1NDIxMDMzIiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjQyMTQyMTc1OCwiZGF0YSI6eyJ0eXBlIjoidGVzdCJ9fQ.Ac4Q2LxKKT8PjvGm0Ry6jr9F-IyR3QpQPwHOsT1_0GTHtjN2GG3QYxh0EEh09hpRujFAhVP4ISmH_sl0ruxz2vmrAM_o_rMejmMndpkRJ1nHKF2SJllJ0T7R7azt9YiZDEEdHdc6oRv2TPtrOALMmQ3X63_9h46Gbzar3rY2DLkbP0kC`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      data,             // ton JSON de variables
+      convertTo: "pdf"  // sortie PDF
+    })
+  });
+
+  if (!res.ok) {
+    const error = await res.text();
+    return NextResponse.json({ error }, { status: res.status });
+  }
+
+  // Retourner le PDF généré comme fichier binaire
+  const pdfBuffer = Buffer.from(await res.arrayBuffer());
+  return new NextResponse(pdfBuffer, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "inline; filename=etude_enedis.pdf"
+    }
+  });
 }
